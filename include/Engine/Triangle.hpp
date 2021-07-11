bc7157407715dcb8c29400da63b4edfed6468224
DipStax <DipStax.hugo@gmail.com>
2021-07-11 22:44:52 +0200

    [ADD]: Triangle class

---
+++ include/Engine/Triangle.hpp
@@ -0,0 +1,24 @@
+/*
+** DipStax 2021
+** 3D-Engine
+** File description:
+** Triangle.hpp
+*/
+
+#pragma once
+
+#include "3D/Point.hpp"
+
+namespace Engine {
+
+    class Triangle {
+        public:
+            Triangle(const d3::Pointf &_fst, const d3::Pointf &_snd, const d3::Pointf &_thd);
+            ~Triangle() = default;
+
+            d3::Pointf operator[](size_t _n);
+
+        private:
+            d3::Pointf m_pt[3];
+    };
+}
---
+++ src/Engine/Triangle.cpp
@@ -0,0 +1,25 @@
+/*
+** DipStax 2021
+** 3D-Engine
+** File description:
+** Triangle.hpp
+*/
+
+#pragma once
+
+#include "Engine/Triangle.hpp"
+
+namespace Engine {
+
+    Triangle::Triangle(const d3::Pointf &_fst, const d3::Pointf &_snd, const d3::Pointf &_thd)
+    {
+        m_pt[0] = _fst;
+        m_pt[1] = _snd;
+        m_pt[2] = _thd;
+    }
+
+    d3::Pointf Triangle::operator[](size_t _n)
+    {
+        return m_pt[_n];
+    }
+}
