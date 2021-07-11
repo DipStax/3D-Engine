165460b0c6f106b3b5c927383b7ce276e18707f7
DipStax <DipStax.hugo@gmail.com>
2021-07-11 22:47:10 +0200

    [ADD]: Camera header (no test)

---
+++ include/Engine/Camera.hpp
@@ -0,0 +1,29 @@
+/*
+** DipStax 2021
+** 3D-Engine
+** File description:
+** Camera
+*/
+
+#pragma once
+
+#include "3D/Point.hpp"
+#include "3D/Vector.hpp"
+
+class Camera {
+    public:
+        Camera(const d3::Pointf &pos, const d3::Pointf &_target = d3::Pointf{ 0, 0, 0 }, const d3::Pointf &_vup = d3::Pointf{ 0, 1, 0 });
+        ~Camera() = default;
+
+        void setVUp(const d3::Pointf &_vup = d3::Pointf{ 0, 1, 0 });
+        void setTarget(const d3::Pointf &_target);
+        void setPosition(const d3::Pointf &_pos);
+
+        void moveTarget(const d3::Pointf &_target);
+        void move(const d3::Pointf &_move);
+
+    private:
+        d3::Pointf m_pos;
+        d3::Pointf m_target;
+        d3::Pointf m_vup;
+};
