3adaf068347e0adcfbc3c89b8a3e75b6886872b3
Hugo PREVOST <hugo.prevost@epitech.eu>
2021-07-13 01:35:12 +0200

    [ADD]: deployment of function: identity, reset, getHeight, getWidth, pointAt, operator*(Matrix, Vector)
    [UP]: size_t -> std::size_t
    
    [FIX]: add arithmetic check

--- include/Maths/Matrix.hpp
+++ include/Maths/Matrix.hpp
@@ -9,31 +9,39 @@
 
 #include <vector>
 
+#include "3D/Vector.hpp"
+
 namespace Maths {
 
     template<typename T>
     class Matrix {
+        static_assert(std::is_arithmetic<T>::value);
         public:
-            Matrix(const size_t _height, size_t _width);
+            Matrix(const std::size_t _height, const std::size_t _width);
             ~Matrix() = default;
 
             void identity();
+            void reset(const T &_val = 0);
 
-            std::vector<T> &operator[](size_t _n);
-            std::vector<T> &at(size_t _n);
-            size_t getHeight() const;
-            size_t getWidth() const;
+            std::vector<T> &operator[](std::size_t _n);
+            const std::vector<T> &at(std::size_t _n) const;
+            std::size_t getHeight() const;
+            std::size_t getWidth() const;
 
-            bool resize(const size_t _height, const size_t _width);
+            bool resize(const std::size_t _height, const std::size_t _width);
             bool checkIntegrity() const;
+
+            void pointAt(const d3::Vectorf &_pos, const d3::Vectorf &_target, const d3::Vectorf &_vup);
         private:
-            size_t m_height;
-            size_t m_width;
-            std::vector<std::vector<T>> m_mtr;
+            std::size_t m_height = 0;
+            std::size_t m_width = 0;
+            std::vector<std::vector<T>> m_mtr = {};
     };
 }
 
 template<typename T>
 Maths::Matrix<T> operator*(const Maths::Matrix<T> &_fst, const Maths::Matrix<T> &_snd);
+template<typename T>
+d3::Vector<T> operator*(const Maths::Matrix<T> &_fst, const d3::Vector<T> &_snd);
 
 #include "Matrix.inl"
