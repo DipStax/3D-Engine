51cc14cf2c0abd4b622000ff6d069620d9ce4bf8
Hugo PREVOST <hugo.prevost@epitech.eu>
2021-07-13 01:34:04 +0200

    [UP]: size_t -> std::size_t

--- include/Engine/Triangle.hpp
+++ include/Engine/Triangle.hpp
@@ -16,7 +16,7 @@ namespace Engine {
             Triangle(const d3::Pointf &_fst, const d3::Pointf &_snd, const d3::Pointf &_thd);
             ~Triangle() = default;
 
-            d3::Pointf operator[](size_t _n);
+            d3::Pointf operator[](std::size_t _n);
 
         private:
             d3::Pointf m_pt[3];
