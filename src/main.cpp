2572074028d11ae2140d98c7b19fd75e3734f8a4
Hugo PREVOST <hugo.prevost@epitech.eu>
2021-08-15 12:30:31 +0200

    [UP]: implementation of a small test

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,30 @@
 */
 
 #include <iostream>
-#include "3D/Point.hpp"
-#include "3D/Vector.hpp"
+#include "Engine/D3Instance.hpp"
 
 int main()
 {
-    d3::Pointi pti2(1, 2, 3);
-    d3::Pointi pti1(2, 3, 4);
+    try {
+        std::cout << "run" << std::endl;
+        Engine::D3Instance instance("test", { 300, 300 });
+        Engine::Camera cam({0, 5, 5});
+        Engine::Mesh mesh;
 
-    d3::Pointi ptr = pti1 - pti2;
-
-    d3::Vectori vec1(1, 2, 3);
-    d3::Vectori vec2(2, 3, 4);
-
-    d3::Vectori vec = vec1 - vec2;
-    std::cout << "run" << std::endl;
+        mesh.setModel(Engine::Mesh::Model::PYRAMID);
+        while (instance.isOpen()) {
+            std::cout << "> reset" << std::endl;
+            instance.reset();
+            std::cout << "| refresh" << std::endl;
+            instance.refresh(cam, mesh);
+            std::cout << "| render" << std::endl;
+            instance.render(mesh);
+            std::cout << "< display" << std::endl;
+            instance.display();
+            std::cout << ">> new frame" << std::endl;
+        }
+    } catch (const char *e) {
+        std::cout << e << std::endl;
+    }
     return 0;
 }
