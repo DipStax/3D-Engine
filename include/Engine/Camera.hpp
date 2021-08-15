c641813f7e351fdc3b3e5f97803addb56ad8c5f1
Hugo PREVOST <hugo.prevost@epitech.eu>
2021-08-15 12:23:55 +0200

    [UP]: full implementation of Camera
    [UP]: use of nodiscard on getter

--- include/Engine/Camera.hpp
+++ include/Engine/Camera.hpp
@@ -2,7 +2,7 @@
 ** DipStax 2021
 ** 3D-Engine
 ** File description:
-** Camera
+** Camera.hpp
 */
 
 #pragma once
@@ -15,6 +15,9 @@ namespace Engine {
         public:
             Camera(const d3::Pointf &_pos, const d3::Pointf &_target = d3::Pointf{ 0, 0, 0 }, const d3::Pointf &_vup = d3::Pointf{ 0, 1, 0 });
             ~Camera() = default;
+
+            [[nodiscard]] d3::Pointf getPosition() const;
+            [[nodiscard]] Maths::Matrix<float> getMatrix() const;
 
             void setVUp(const d3::Pointf &_vup = d3::Pointf{ 0, 1, 0 });
             void setTarget(const d3::Pointf &_target);
@@ -29,5 +32,7 @@ namespace Engine {
             d3::Pointf m_pos;
             d3::Pointf m_target;
             d3::Pointf m_vup;
+            d3::Vectorf m_dir;
+            Maths::Matrix<float> m_mtr;
     };
 }
--- src/Engine/Camera.cpp
+++ src/Engine/Camera.cpp
@@ -10,16 +10,59 @@
 namespace Engine {
 
     Camera::Camera(const d3::Pointf &_pos, const d3::Pointf &_target, const d3::Pointf &_vup)
-        : m_pos(_pos), m_target(_target), m_vup(_vup)
+        : m_pos(_pos), m_target(_target), m_vup(_vup), m_dir(0, 0, 0), m_mtr(4, 4)
     {
         calculate();
     }
 
+    d3::Pointf Camera::getPosition() const
+    {
+        return m_pos;
+    }
+
+    Maths::Matrix<float> Camera::getMatrix() const
+    {
+        return m_mtr;
+    }
+
+    void Camera::setVUp(const d3::Pointf &_vup)
+    {
+        m_vup = _vup;
+        calculate();
+    }
+
+    void Camera::setTarget(const d3::Pointf &_target)
+    {
+        m_target = _target;
+        calculate();
+    }
+
+    void Camera::setPosition(const d3::Pointf &_pos)
+    {
+        m_pos = _pos;
+        calculate();
+    }
+
+    void Camera::moveTarget(const d3::Pointf &_target)
+    {
+        m_target = m_target + _target;
+        calculate();
+    }
+
+    void Camera::move(const d3::Pointf &_move)
+    {
+        m_pos = m_pos + _move;
+        calculate();
+    }
+
+
     void Camera::calculate()
     {
-        d3::Vectorf target = (Maths::matrixRotationY(0) * d3::Vectorf(m_target)) + d3::Vectorf(m_pos);
-        Maths::Matrix<float> mtr(4, 4);
+        d3::Vectorf target;
 
-        mtr.pointAt(m_pos, target, m_vup);
+        m_dir = Maths::matrixRotationY(0) * d3::Vectorf(m_target);
+        target = d3::Vectorf(m_pos) + m_dir;
+        m_mtr.pointAt(m_pos, target, m_vup);
+        m_mtr.quickInvert();
     }
 }
