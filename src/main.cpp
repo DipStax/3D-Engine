798ef01ae076cdba1d169ceb5c003b97c5df96d4
DipStax <DipStax.hugo@gmail.com>
2021-09-02 01:30:57 +0200

    [FIX]: change camera position
    [DEL]: useless debug print

--- src/main.cpp
+++ src/main.cpp
@@ -14,22 +14,16 @@ int main()
     try {
         std::cout << "run" << std::endl;
         Engine::D3Instance instance("test", { 500, 400 });
-        Engine::Camera cam({0, 0, 0}, {0, 0, 1});
+        Engine::Camera cam({2, 2, -3}, {0, 0, 1});
         Engine::Mesh mesh;
         std::string str;
 
         mesh.setModel(Engine::Mesh::Model::CUBE);
         while (instance.isOpen()) {
-            cam.move({0.1, 0, 0});
-            std::cout << "> reset" << std::endl;
             instance.reset();
-            std::cout << "| refresh" << std::endl;
             instance.refresh(cam, mesh);
-            std::cout << "| render" << std::endl;
             instance.render(mesh);
-            std::cout << "< display" << std::endl;
             instance.display();
-            std::cout << ">> new frame" << std::endl;
             Sleep(100);
         }
     } catch (const char *e) {
