405703d560766ea92724ea241f0d2b96b416de0c
DipStax <DipStax.hugo@gmail.com>
2021-09-01 19:37:48 +0200

    [UP]: add test of cube mesh and transform function

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,20 @@
 
 #include <iostream>
 #include "Engine/D3Instance.hpp"
+#include <Windows.h>
 
 int main()
 {
     try {
         std::cout << "run" << std::endl;
         Engine::D3Instance instance("test", { 500, 400 });
-        Engine::Camera cam({0, 5, 5});
+        Engine::Camera cam({0, 0, 0}, {0, 0, 1});
         Engine::Mesh mesh;
         std::string str;
 
-        mesh.setModel(Engine::Mesh::Model::PYRAMID);
+        mesh.setModel(Engine::Mesh::Model::CUBE);
         while (instance.isOpen()) {
+            cam.move({0.1, 0, 0});
             std::cout << "> reset" << std::endl;
             instance.reset();
             std::cout << "| refresh" << std::endl;
@@ -28,7 +30,7 @@ int main()
             std::cout << "< display" << std::endl;
             instance.display();
             std::cout << ">> new frame" << std::endl;
-            std::cin >> str;
+            Sleep(100);
         }
     } catch (const char *e) {
         std::cout << e << std::endl;
